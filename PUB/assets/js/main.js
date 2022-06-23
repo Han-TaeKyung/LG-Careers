e3548102c4b4db0175174b0ab70e8b384988ad8c
shin <finetonight@gmail.com>
2022-06-23 13:14:26 +0900

    main ing...

--- PUB/assets/js/main.js
+++ PUB/assets/js/main.js
@@ -1,40 +1,63 @@
 $(function () {
-    //마우스 스크롤
-    if ($(window).width() > 1199) {
-        $('.page').mousewheel(function (e, delta) {//플러그인 호출 후 사용가능
-            h = $(window).height();
-            page = parseInt($(this).attr('data-page')) - delta;
-            $('body,html').stop().animate({'scrollTop': (page - 1) * h}, 1000);
-            return false;//기본마우스 휠 기능 제거
-        });
+  var $window = $(window),
+    $html = $("html");
+
+  var $container = $("#container"),
+    $conSection = $container.find(" > section"),
+    sectionLength = $conSection.length,
+    sectionRate = 1 / sectionLength,
+    $mainNavItem = $(".mainNav li");
+
+  //마우스 스크롤
+  if ($window.width() > 1199) {
+    $(".page").mousewheel(function (e, delta) {
+      //플러그인 호출 후 사용가능
+      h = $window.height();
+      page = parseInt($(this).attr("data-page")) - delta;
+      if (page) {
+        var dataPage;
+        dataPage = page <= 0 ? 1 : page;
+        dataPage = page >= sectionLength ? 4 : page;
+        $html.attr("data-page", dataPage);
+      }
+      $("body,html")
+        .stop()
+        .animate({ scrollTop: (page - 1) * h }, 100);
+      handleNav();
+      return false; //기본마우스 휠 기능 제거
+    });
+  }
+  function handleNav() {
+    var nav = $html.attr("data-page");
+  }
+  //스크롤이벤트
+  $(".mainNav li").click(function () {
+    //스크롤 애니메이션
+    n = $(this).attr("data-n");
+    target = $(".page" + n).offset().top;
+    $("body,html").stop().animate({ scrollTop: target }, 100);
+  });
+  $window.scroll(function () {
+    //스크롤 이벤트가 발생했을때
+    st = $window.scrollTop(); //수직스크롤 값을 가져오기
+    h = $window.height();
+    offset2 = $(".page2").offset().top;
+    offset3 = $(".page3").offset().top;
+    offset4 = $(".page4").offset().top;
+
+    $mainNavItem.removeClass("mainOn");
+    if (st < offset2 - h * 0.25) {
+      //1page조건
+      $(".mainNav01").addClass("mainOn");
+    } else if (st < offset3 - h * 0.5) {
+      //2page조건
+      $(".mainNav02").addClass("mainOn");
+    } else if (st < offset4 - h * 0.75) {
+      //3page조건
+      $(".mainNav03").addClass("mainOn");
+    } else if (st >= offset4 - h * 0.75) {
+      //4page조건
+      $(".mainNav04").addClass("mainOn");
     }
-
-    //스크롤이벤트
-    $('.mainNav li').click(function(){
-        //스크롤 애니메이션
-        n=$(this).attr('data-n');
-        target=$('.mainPage0'+n).offset().top;
-        $('body,html').stop().animate({'scrollTop':target},700)
-    });
-    $(window).scroll(function(){//스크롤 이벤트가 발생했을때
-        st=$(window).scrollTop();//수직스크롤 값을 가져오기
-        h=$(window).height();
-        offset2=$('.mainPage02').offset().top;
-        offset3=$('.mainPage03').offset().top;
-        offset4=$('.mainPage04').offset().top;
-
-        $('.mainNav li').removeClass('mainOn');
-        if(st<offset2-h*0.25){//1page조건
-            $('.mainNav01').addClass('mainOn')
-        }
-        if(st>=offset2-h*0.25 && st<offset3-h*0.5){//2page조건
-            $('.mainNav02').addClass('mainOn')
-        }
-        if(st>=offset3-h*0.5 && st<offset4-h*0.75){//3page조건
-            $('.mainNav03').addClass('mainOn')
-        }
-        if(st>=offset4-h*0.75){//4page조건
-            $('.mainNav04').addClass('mainOn')
-        }
-    });
+  });
 });
